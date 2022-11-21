6b2d26845ef1375dcc6bd852783f0299564f12ee
Ian Holsteen <ianholsteen@gmail.com>
2022-11-21 15:04:49 -0800

    commit tree 1d3090719602b2c102af61d0378d5753cfd189c3

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,6 +1,29 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
 
+// src/ColoradoStateParks.js
+import * as RMFunctions from "./parks/RockyMountain";
+
+import {
+  trees as parkTrees,
+  wildlife as parkWildlife,
+} from "./parks/RockyMountain";
+
+console.log(parkTrees);
+// > "Aspen and Pine"
+
+parkWildlife();
+// > "Elk, Bighorn Sheep, Moose"
+
+console.log(RMFunctions.trees);
+// => "Aspen and Pine"
+
+RMFunctions.wildlife();
+// => "Elk, Bighorn Sheep, Moose"
+
+RMFunctions.elevation();
+// => Attempted import error
+
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
